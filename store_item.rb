7bbe29a9c5444df9a0115cab2385272ac79db68a
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-05-02 12:40:03 -0700

    Initial

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+speaker = { color: "red", price: "70", size: "medium" }
+tv = { color: "black", price: "1000", size: "large" }
+mouse = { color: "white", price: "30", size: "small" }
+
+puts "#{speaker[:color]} #{speaker[:price]} #{speaker[:size]}"
+puts "#{tv[:color]} #{tv[:price]} #{tv[:size]}"
+puts "#{mouse[:color]} #{mouse[:price]} #{mouse[:size]}"
