413a7df461e8b280f134836569ec76f13ee3fa14
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-05-05 08:46:21 -0700

    base class code

--- module_example.rb
+++ module_example.rb
@@ -21,24 +21,7 @@ class Car
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = "north"
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Car
   def ring_bell
     puts "Ring ring!"
   end
